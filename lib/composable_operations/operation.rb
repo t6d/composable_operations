f6d0288073028e652cbbb08bb762e55f9d482500
Konstantin Tennhard <me@t6d.de>
2012-11-30 16:58:58 +0100

    Initial operation implementation

---
+++ lib/composable_operations/operation.rb
@@ -0,0 +1,105 @@
+class Operation
+  class << self
+
+    def default_configuration
+    end
+
+    def execute(data, &configuration)
+      new(&configuration).execute(data)
+    end
+
+    protected
+
+      def event(name)
+        mod = if const_defined?(:Events, false)
+          const_get(:Events)
+        else
+          new_mod = Module.new do
+            def self.to_s
+              "Events(#{instance_methods(false).join(', ')})"
+            end
+          end
+          const_set(:Events, new_mod)
+        end
+
+        mod.module_eval do
+          define_method(name) do |*args|
+            notify_subscribers(name, *args)
+          end
+        end
+
+        include mod
+      end
+
+  end
+
+  event :will_execute
+  event :will_execute_operational_unit
+  event :did_execute_operational_unit
+  event :did_execute
+
+  def initialize(&configuration)
+    @operational_units = []
+
+    configuration ||= self.class.default_configuration
+    configuration.call(self) unless configuration.nil?
+  end
+
+  def subscribers
+    @subscribers.dup
+  end
+
+  def subscribe(subscriber)
+    (@subscribers ||= []) << subscriber
+  end
+
+  def unsubscribe(subscriber)
+    (@subscribers || []).remove(subscriber)
+  end
+
+  def use(operational_unit)
+    operational_units.push(operational_unit)
+  end
+
+  def call(data)
+    return true if operational_units.empty?
+
+    will_execute(data)
+
+    success = true
+    operational_units.each do |operational_unit|
+      if !execute_operational_unit(operational_unit, data)
+        success = false
+        break
+      end
+    end
+
+    did_execute(data, success)
+
+    success
+  end
+  alias execute call
+
+  protected
+
+    attr_reader :operational_units
+
+    def execute_operational_unit(operational_unit, data)
+      operational_unit = instantiate(operational_unit)
+      will_execute_operational_unit(operational_unit, data)
+      success = !!operational_unit.call(data)
+      did_execute_operational_unit(operational_unit, data, success)
+      success
+    end
+
+    def instantiate(value)
+      value.is_a?(Class) ? value.new : value
+    end
+
+    def notify_subscribers(event, *args)
+      (@subscribers || []).each do |subscriber|
+        subscriber.public_send("on_#{event}", self, *args)
+      end
+    end
+
+end
---
+++ spec/composable_operations/operation_spec.rb
@@ -0,0 +1,128 @@
+require 'spec_helper'
+
+describe Operation do
+
+  let(:operation) { described_class.new }
+
+  subject { operation }
+
+  it "should provide a convenience method for execution" do
+    described_class.execute('').should be == true
+  end
+
+  context "when provided with an empty configuration block" do
+
+    specify "the preparation should be successful" do
+      operation.execute('').should be_true
+    end
+
+  end
+
+  context "when provided with an operational unit that always fails" do
+
+    before do
+      operation.use lambda { |data| false }
+    end
+
+    specify "the preparation should not be successful" do
+      operation.execute('').should == false
+    end
+
+    context "when instrumented with a simple logger" do
+
+      let(:logger) do
+        class << (logger = Object.new.extend(OperationTracking))
+          attr_reader :result
+
+          def on_did_execute(sender, data, success)
+            @result = success ? "Preparation succeeded" : "Preparation failed"
+          end
+        end
+
+        logger
+      end
+
+      before do
+        operation.subscribe(logger)
+        operation.execute('')
+      end
+
+      specify "the logger should have stored the correct result" do
+        logger.result.should be == "Preparation failed"
+      end
+
+    end
+
+  end
+
+  context "when using an operational unit that checks whether or not the given data contains the phrase 'chunky bacon'" do
+
+    let(:operational_unit) do
+      Class.new(OperationalUnit) do
+
+        def self.name
+          "ChunkyBaconChecker"
+        end
+
+        def execute(data)
+          fail("the data does not contain the phrase 'chunky bacon'") unless !!(/chunky bacon/.match(data))
+          succeed
+        end
+
+      end
+    end
+
+    let(:logger) do
+      class << (logger = Object.new.extend(OperationTracking))
+
+        def messages
+          @messages ||= []
+        end
+
+        def on_did_execute_operational_unit(sender, operational_unit, data, success)
+          messages << [operational_unit.description, (success ? 'succeeded' : "failed because #{operational_unit.reason}")].join(' ')
+        end
+
+      end
+
+      logger
+    end
+
+    let(:last_message) { logger.messages.last }
+
+    before do
+      operation.subscribe(logger)
+      operation.use operational_unit
+    end
+
+    context "when the processed data does not contain the correct phrase" do
+
+      let!(:result) { operation.execute('crispy bacon') }
+
+      specify "the preparation should be not successful" do
+        result.should be == false
+      end
+
+      specify "the last log message should state that something went wrong" do
+        last_message.should be == "Chunky bacon checker failed because the data does not contain the phrase 'chunky bacon'"
+      end
+
+    end
+
+    context "when the processed data contains the correct phrase" do
+
+      let!(:result) { operation.execute('chunky bacon') }
+
+      specify "the preparation should be successful" do
+        result.should be == true
+      end
+
+      specify "the last log message should state that everything went well" do
+        last_message.should be == 'Chunky bacon checker succeeded'
+      end
+
+    end
+
+  end
+
+end
