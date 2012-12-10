bf5c805b16c013092d0322deb5e25a0291f0173e
Konstantin Tennhard <me@t6d.de>
2012-12-10 13:25:16 +0100

    Adds specs for operation finalization and preparation

--- spec/composable_operations/operation_spec.rb
+++ spec/composable_operations/operation_spec.rb
@@ -50,6 +50,37 @@ describe Operation do
       simple_operation.should be_succeeded
     end
 
+    context "when provided with a preparator" do
+      let(:logger) { stub('Logger') }
+
+      before do
+        logger = logger()
+        simple_operation.before do
+          logger.info("Preparing")
+        end
+      end
+
+      it "should invoke the preparator" do
+        logger.should_receive(:info)
+        simple_operation.perform
+      end
+    end
+
+    context "when provided with a finalizer" do
+      let(:logger) { stub('Logger') }
+
+      before do
+        logger = logger()
+        simple_operation.after do
+          logger.info("Finalizing")
+        end
+      end
+
+      it "should invoke the finalizer" do
+        logger.should_receive(:info)
+        simple_operation.perform
+      end
+    end
   end
 
 end
