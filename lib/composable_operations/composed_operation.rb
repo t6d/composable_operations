37e289c7a4b85bd99df9647ee26e3396ee6bfa85
Konstantin Tennhard <me@t6d.de>
2013-03-19 19:16:06 +0100

    Transition callbacks for operations
    Composed operations now support callbacks for that are triggered when a transition is made from one pipeline element to the next.

--- lib/composable_operations/composed_operation.rb
+++ lib/composable_operations/composed_operation.rb
@@ -23,6 +23,24 @@ class ComposedOperation < Operation
       end
     end
 
+    def transitions
+      transitions = []
+      klass = self
+      while klass != Operation
+        klass = klass.superclass
+        transitions += Array(klass.instance_variable_get(:@transitions))
+      end
+      transitions += Array(@transitions)
+      transitions
+    end
+
+
+    protected
+
+      def between(&callback)
+        (@transitions ||= []) << callback
+      end
+
   end
 
   def operations
@@ -32,18 +50,27 @@ class ComposedOperation < Operation
   protected
 
     def execute
-      operations.inject(input) do |data, operation|
-        operation = operation.new(data)
-        operation.perform
+      [nil, *operations, nil].each_cons(2).inject(input) do |data, operations|
+        if operation = operations.last
+          operation = operation.new(data)
+          operation.perform
 
-        if operation.failed?
-          fail operation.message
-        elsif operation.halted?
-          halt operation.message, operation.result
+          if operation.failed?
+            fail operation.message
+          elsif operation.halted?
+            halt operation.message, operation.result
+          end
+
+          transition(*operations, data) if operations.first && operations.last
+          operation.result
+        else
+          data
         end
-
-        operation.result
       end
+    end
+
+    def transition(a, b, payload)
+      self.class.transitions.each { |transition| instance_exec(a, b, payload, &transition) }
     end
 
 end
--- spec/composable_operations/composed_operation_spec.rb
+++ spec/composable_operations/composed_operation_spec.rb
@@ -4,6 +4,10 @@ describe ComposedOperation do
 
   let(:string_generator) do
     Class.new(Operation) do
+      def self.name
+        "StringGenerator"
+      end
+
       def execute
         "chunky bacon"
       end
@@ -12,6 +16,10 @@ describe ComposedOperation do
 
   let(:string_capitalizer) do
     Class.new(Operation) do
+      def self.name
+        "StringCapitalizer"
+      end
+
       def execute
         input.upcase
       end
@@ -91,6 +99,37 @@ describe ComposedOperation do
     end
 
     it { should utilize_operations(string_generator, halting_operation, string_capitalizer) }
+  end
+
+  context "when composed of two operations and provided with a between block" do
+
+
+    let(:logger) { stub("Logger").as_null_object }
+
+    subject(:composed_operation) do
+      string_generator = string_generator()
+      string_capitalizer = string_capitalizer()
+      logger = logger()
+
+      operation = described_class.compose do
+        use string_generator
+        use string_capitalizer
+
+        between do |a, b, payload|
+          logger.info("#{a.name} -> #{b.name} with #{payload.inspect} as payload")
+        end
+      end
+
+      operation.new
+    end
+
+    it { should succeed_to_perform.and_return("CHUNKY BACON") }
+
+    it "should generate the correct log message" do
+      logger.should_receive(:info).with("StringGenerator -> StringCapitalizer with \"chunky bacon\" as payload")
+      composed_operation.perform
+    end
+
   end
 
 end
