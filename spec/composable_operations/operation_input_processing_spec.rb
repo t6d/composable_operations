ab10192003668f36845667a9d9ca5212cbb988c8
Konstantin Tennhard <me@t6d.de>
2013-08-02 15:21:39 +0200

    Adds an outline for the different testing scenarios regarding the new operation input handling

---
+++ spec/composable_operations/operation_input_processing_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "Operation input processing:" do
+
+  describe "An operation that takes a Hash as input" do
+  end
+
+  describe "An operation that takes a Hash as input and an Hash of additional options" do
+  end
+
+  describe "An operation that takes multiple arguments as input" do
+  end
+
+  describe "An operation that takes multiple arguments as input where the last of these arguments is a Hash" do
+  end
+
+  describe "An operation that takes multiple arguments as input where the last of these arguments is a Hash, as well as, a Hash of additional options" do
+  end
+
+end
